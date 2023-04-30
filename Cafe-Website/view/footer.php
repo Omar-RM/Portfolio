43c969f477e8d2ed0600b5e1861260d433c366f9
Red-Dragn <omarm.rico.98@gmail.com>
2023-04-30 20:41:13 +0100

    view and style

---
+++ Cafe-Website/view/footer.php
@@ -0,0 +1,21 @@
+
+</main>
+    </div>
+
+
+    <footer>
+        <script>
+            var today = new Date();
+            var date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+            var dateBox = document.getElementById("date-text");
+            dateBox.textContent = date;
+        </script>
+            <script src="../view/app.js"></script>
+
+        
+    </footer>
+
+
+</body>
+
+</html>
---
+++ Cafe-Website/view/nav.php
@@ -0,0 +1,24 @@
+<header>
+    <nav>
+        <div class="nav-bar">
+            <div class="box home-btn">
+                <h3>TAZAS ROTAS
+                </h3>
+            </div>
+            <div class="box search-bar" aria-placeholder="Q">to search</div>
+            <div class="box user">
+                <div id="date">
+                    <span class="date-text" id="date-text"></span>
+                </div>
+                <div class="username"></div>
+            </div>
+            <div class="box venta-btn"><img class="icon" src="sell.png"></div>
+            <div class="box clock-btn"><img class="icon" src="clock.png"></div>
+            <div class="box options"><img class="icon" src="settings.png"></div>
+        </div>
+    </nav>
+    <aside id="submenu">
+        <div id="prod-list"><img class="icon" src="sell.png"></div>
+        <div id="ventas"></div>
+    </aside>
+</header>
